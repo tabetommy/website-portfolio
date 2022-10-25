cd175e71c036ea8ea7da6863294b45a1535d7fc2
Tommy Egbe <tabe_e@yahoo.ca>
2022-10-26 01:54:37 +0200

    refactored whole project

--- js/script.js
+++ js/script.js
@@ -82,3 +82,51 @@ let formValidation=(
 	}
 
 	)();
+
+
+	function reveal() {
+		var reveals = document.querySelectorAll(".projects-card");
+	  
+		for (var i = 0; i < reveals.length; i++) {
+		  var windowHeight = window.innerHeight;
+		  var elementTop = reveals[i].getBoundingClientRect().top;
+		  var elementVisible = 150;
+	  
+		  if (elementTop < windowHeight - elementVisible) {
+			reveals[i].classList.add("active");
+		  } else {
+			reveals[i].classList.remove("active");
+		  }
+		}
+	  }
+	  
+	  window.addEventListener("scroll", reveal);
+
+// (function($){
+// 	$(".projects-card").each(function(index){
+		
+// 	})
+// })(jQuery)
+
+// handle hamburger menu toggle
+(function($){
+	$('.menu-close').hide();
+	$('.nav-bar-toggle').hide();
+	$('.ham-menu-icon').click(function(){
+		$('.nav-bar-toggle').slideToggle('slow',function(){
+			$('.ham-menu-icon').hide();
+			$('.menu-close').show()
+		});
+	});
+
+	$('.menu-close').click(function() {
+	$( ".nav-bar-toggle" ).slideToggle( 'slow', function() {
+	$( ".menu-close" ).hide();
+	$( ".ham-menu-icon" ).show();
+	});
+	});
+
+})(jQuery)
+
+
+
